5a9aa6cf61996fbdd22588738205f0492bc03ddb
Davide Angelocola <davide.angelocola@exmachina.ch>
2014-11-29 19:20:08 +0100

    dev: array->array mapper

--- src/main/javascript/fungoid.js
+++ src/main/javascript/fungoid.js
@@ -2,4 +2,45 @@
 
 var Fungoid = {
 
+	array_input_iterator: function(array) {
+		var i = 0;
+		return function() {
+			if (i < array.length) {
+				return {
+					value: array[i++],
+					done: false
+				};
+			} else {
+				return {
+					done: true
+				};
+			}
+		};
+	},
+
+	array_output_iterator: function() {
+		var output = [];
+		return function(e) {
+			output.push(e);
+			return output;
+		};
+	},
+
+	mapper: function(fn) {
+		return function(e) {
+			return fn(e);
+		};
+	},
+
+	transform: function(input, fn, output) {
+		for (;;) {
+			var it = input();
+			if (!it.done) {
+				var res = output(fn(it.value));
+			} else {
+				break;
+			}
+		}
+		return res;
+	}
 };
--- src/test/javascript/fungoid-test.js
+++ src/test/javascript/fungoid-test.js
@@ -1,4 +1,13 @@
 'use strict';
-describe("fungoid", function() {
 
+describe("fungoid", function() {
+	it('transform array', function() {
+		var input = [ 1, 2, 3 ];
+		var output = Fungoid.transform(
+			Fungoid.array_input_iterator(input),
+			Fungoid.mapper(function(x) { return x + 1; }),
+			Fungoid.array_output_iterator()
+		);
+		expect(output).toEqual([ 2, 3, 4]);
+	});
 });
