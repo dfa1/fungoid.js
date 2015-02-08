cd686492f045bc1d2a786494acee48f99a7cc640
Davide Angelocola <davide.angelocola@exmachina.ch>
2015-02-08 12:38:31 +0100

    ref: reindent

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,32 +12,32 @@ var jasmineOptions = {
 
 gulp.task('build', function () {
     return gulp.src('src/fungoid.js')
-	.pipe(to5())
-	.pipe(umd({
-		exports: function(file)   { return 'exports';},
-		namespace: function(file) { return 'Fungoid'; }
-	}))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-	.pipe(gulp.dest('build'));
+		.pipe(to5())
+		.pipe(umd({
+			exports: function(file)   { return 'exports';},
+			namespace: function(file) { return 'Fungoid'; }
+		}))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(gulp.dest('build'));
 });
 
 gulp.task('test', [ 'build' ], function () {
 	return gulp.src(['test/test-fungoid.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-	.pipe(jasmine(jasmineOptions));
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jasmine(jasmineOptions));
 });
 
 gulp.task('coverage', [ 'build' ], function () {
 	return gulp.src(['test/test-fungoid.js'])
-	.pipe(cover.instrument({
-		pattern: ['build/fungoid.js'],
-		debugDirectory: 'build/debug'
-	}))
-	.pipe(jasmine())
-	.pipe(cover.gather())
-	.pipe(cover.format())
-	.pipe(gulp.dest('build/reports'));
+		.pipe(cover.instrument({
+			pattern: ['build/fungoid.js'],
+			debugDirectory: 'build/debug'
+		}))
+		.pipe(jasmine())
+		.pipe(cover.gather())
+		.pipe(cover.format())
+		.pipe(gulp.dest('build/reports'));
 });
 
